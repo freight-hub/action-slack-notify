f104e329d41535d81accb1c9657068109902c104
Lennart Querter <lennart.querter@gmail.com>
2022-11-30 16:28:01 +0100

    feat: send message to slack

---
+++ src/main.js
@@ -0,0 +1,47 @@
+const core = require('@actions/core')
+const axios = require('axios')
+
+async function run() {
+    try {
+        await execute()
+    } catch (error) {
+        core.setFailed(error.message)
+    }
+}
+
+async function execute() {
+    const {slackWebhookUrl, message, isMarkdown} = getAndValidateInput()
+    let result;
+    try {
+        result = await axios.post(slackWebhookUrl, {
+            text: message,
+            mrkdwn: isMarkdown
+        })
+    } catch (e) {
+        throw new Error(`Could not post message: ${e.message}`)
+    }
+    if (result.status !== 200) {
+        throw new Error(`Message status was not 200. got ${result.status}`)
+    }
+}
+
+
+function getAndValidateInput() {
+    const slackWebhookUrl = core.getInput('slack_webhook_url', {required: true})
+    const message = core.getInput('message', {required: true})
+    let isMarkdown = core.getInput('is_markdown', {required: false})
+
+    if (!isMarkdown) {
+        isMarkdown = false;
+    }
+
+    return {
+        slackWebhookUrl,
+        message,
+        isMarkdown
+    }
+}
+
+;(async () => {
+    await run()
+})()
